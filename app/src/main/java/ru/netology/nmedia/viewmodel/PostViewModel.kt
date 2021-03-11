8945ea24b4be70270d25d38778608f3f84f87a09
alexeis1 <alexei-s1@yandex.ru>
2021-03-11 03:12:25 +0300

    вариант mvvm

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -5,16 +5,25 @@ import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
 data class PostContext(
-    val repository : PostRepository? = null
+    var repository : PostRepository? = null
 )
 
 class PostViewModel(context : PostContext) : ViewModel() {
+    private val repository: PostRepository
     /**
      * Если активити создается в первый раз, то создаем модель.
      * иначе используем данные существующего контекста
      */
-    private val repository: PostRepository = context.repository ?: PostRepositoryInMemoryImpl()
+    init {
+        if (context.repository == null) {
+            context.repository = PostRepositoryInMemoryImpl()
+        }
+        repository = context.repository!!
+    }
+
     val data                    = repository.get()
     fun like(isLiked : Boolean) = repository.like(isLiked)
     fun share()                 = repository.share()
+
+
 }
